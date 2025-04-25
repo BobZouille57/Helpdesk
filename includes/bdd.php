8dce3a027ac7d8c534d84a5602a93ca3d35cc538
BobZouille57 <RaiiZeR57@gmail.com>
2025-04-25 14:50:34 +0200

    Modif style

--- includes/bdd.php
+++ includes/bdd.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 try {
-    $pdo = new PDO("mysql:host=127.0.0.1;port=3308;dbname=helpdesk;charset=utf8", "root", "");
+    $pdo = new PDO("mysql:host=localhost;port=3308;dbname=clement_db;charset=utf8", "clement", "Clement2003+");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("❌ Erreur de connexion : " . $e->getMessage());
